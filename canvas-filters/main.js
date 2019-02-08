e0eb08bde0937e80da0ac81fc9f6ed1b5293fee8
Mircea Tanasa <tam@officetimeline.com>
2019-02-08 12:19:16 +0200

    add filters progress

--- canvas-filters/main.js
+++ canvas-filters/main.js
@@ -8,6 +8,42 @@ var canvas,
     image,
     imageData;
 
+var availableFilters = [
+  {
+    filterFunction: makeGrayscale,
+    name: "Grayscale"
+  }, {
+    filterFunction: makeSepia,
+    name: "Sepia"
+  }, {
+    filterFunction: makeThreshold,
+    options: {
+      limit: 150
+    },
+    name: "Threshold"
+  }, {
+    filterFunction: makeInvert,
+    name: "Invert"
+  }, {
+    filterFunction: adjustBrightness,
+    options: {
+      change: 50
+    },
+    name: "Lighten"
+  }, {
+    filterFunction: adjustBrightness,
+    options: {
+      change: -50
+    },
+    name: "Darken"
+  }, {
+    filterFunction: makeBlur,
+    options: {
+      change: 2
+    },
+    name: "Blur"
+  }
+]
 
 function initCanvas() {
   canvas = document.getElementById("myCanvas");
@@ -17,12 +53,6 @@ function initCanvas() {
   image = document.getElementById("bamboo");
   canvasContext.drawImage(image, 0, 0, config.canvasWidth, config.canvasHeight);
   imageData = canvasContext.getImageData(0, 0, config.canvasWidth, config.canvasHeight);
-
-  // applyFilter(makeGrayscale, imageData);
-  // applyFilter(adjustBrightness, imageData, {
-  //   change: 50
-  // });
-  applyFilter(makeSepia, imageData);
 }
 
 function applyFilter(filterFunction, data, options) {
@@ -33,10 +63,10 @@ function applyFilter(filterFunction, data, options) {
 function makeGrayscale(pixels) {
   var data = pixels.data;
   for (var i=0; i<data.length; i+=4) {
-    var r = data[i];
-    var g = data[i+1];
-    var b = data[i+2];
-    var mutatedColors = .5*r + .5*g + .5*b;
+    var red = data[i];
+    var green = data[i+1];
+    var blue = data[i+2];
+    var mutatedColors = .5 * red + .5 * green + .5 * blue;
     data[i] = data[i+1] = data[i+2] = mutatedColors
   }
   return pixels;
@@ -55,14 +85,74 @@ function adjustBrightness(pixels, options) {
 function makeSepia(pixels) {
   var data = pixels.data;
   for (var i=0; i<data.length; i+=4) {
-    data[i+2] = 0;
-    var r = data[i];
-    var g = data[i+1];
-    var b = data[i+2];
+    var red = data[i];
+    var green = data[i+1];
+    var blue = data[i+2];
+
+    data[i] = (red * .393) + (green *.769) + (blue * .189)
+    data[i + 1] = (red * .349) + (green *.686) + (blue * .168)
+    data[i + 2] = (red * .272) + (green *.534) + (blue * .131)
   }
   return pixels;
 }
 
+function makeThreshold(pixels, options) {
+  var data = pixels.data;
+  for (var i=0; i<data.length; i+=4) {
+    var red = data[i];
+    var green = data[i+1];
+    var blue = data[i+2];
+
+    if (red + green + blue < options.limit) {
+      data[i] = 0;
+      data[i + 1] = 0;
+      data[i + 2] = 0;
+    } else {
+      data[i] = 255;
+      data[i + 1] = 255;
+      data[i + 2] = 255;
+    }
+  }
+  return pixels;
+}
+
+function makeInvert(pixels) {
+  var data = pixels.data;
+  for (var i=0; i<data.length; i+=4) {
+    data[i] = 255 - data[i];
+    data[i + 1] = 255 - data[i + 1];
+    data[i + 2] = 255 - data[i + 2];
+  }
+  return pixels;
+}
+
+function makeBlur(pixels, options) {
+  var data = pixels.data;
+  for (var i=0; i<data.length; i+=4) {
+    data[i] = 255 - data[i];
+    data[i + 1] = 255 - data[i + 1];
+    data[i + 2] = 255 - data[i + 2];
+  }
+  return pixels;
+}
+
+function createButton(config) {
+  var btn = document.createElement("button");
+  btn.innerText = config.name;
+  btn.addEventListener("click", applyFilter.bind(this, config.filterFunction, imageData, config.options));
+  return btn;
+}
+
+function initButtons() {
+  var wrapper = document.getElementById("filter-wrapper");
+
+  for (let i = 0; i < availableFilters.length; i++) {
+    var button = createButton(availableFilters[i]);
+    wrapper.appendChild(button);
+  }
+}
+
 window.onload = function() {
   initCanvas();
+  initButtons();
 }
